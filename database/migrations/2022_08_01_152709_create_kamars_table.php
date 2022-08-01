bed097429ee2f76777483d7489a29c875f32a655
ARIQFADHLURRAHMAN <fadhlurrahmanariq51@gmail.com>
2022-08-01 22:46:00 +0700

    crud table hotel

---
+++ database/migrations/2022_08_01_152709_create_kamars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKamarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kamars', function (Blueprint $table) {
+            $table->id();
+            $table->string('jeniskamar');
+            $table->string('fasilitaskamar');
+            $table->date('reservasi');
+            $table->string('harga');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kamars');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KamarController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layout.main');
 });
+
+Route::get('/', [KamarController::class, 'index'])->name('kamar');
